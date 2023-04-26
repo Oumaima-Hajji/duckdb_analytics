18f0922d4d68f20cb3bf0b2a40e9d67622ef8691
Oumaima Hajji <oumhjj@gmail.com>
2023-04-26 21:42:05 +0200

    updated files

--- api_call.py
+++ api_call.py
@@ -1,36 +1,21 @@
 import requests
 import duckdb
 import json
-import pandas as pd
 
-api_key = "YOUR API KEY HERE"
-url = f"https://api.nasa.gov/insight_weather/?api_key={api_key}&feedtype=json&ver=1.0"
-
+url = 'https://api.exchangerate.host/2023-04-04'
 response = requests.get(url)
+data = response.json()
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Extract the JSON data from the response
-    data = response.json()
-
-    # Do something with the data, for example print it
-    print(data.keys())
-    
-
-# Convert JSON dictionary to a string
-json_string = json.dumps(data)
-
-# Load JSON string into a Pandas dataframe
-df = pd.json_normalize(json.loads(json_string))
+with open("data.json", "w") as f:
+    json.dump(data, f)
 
 
-#print(df.head(4))
-# Connect to the DuckDB database
-con = duckdb.connect(database='APIdatabase.db')
-# Load dataframe into a DuckDB table
-con.register('mars_weather', df)
+con = duckdb.connect(database='targetapidatabase.db')
 
-# Query the table
-result = con.execute("SELECT * FROM mars_weather").fetchall()
+
+query = "CREATE TABLE exchangerate AS SELECT * FROM read_json_auto('data.json')"
+con.execute(query)
+
+
+result = con.execute("SELECT rates.MAD FROM exchangerate ").fetchall()
 print(result)
-
--- main.py
+++ main.py
@@ -4,35 +4,19 @@ import requests
 # CSV FILE
 
 # specify the file paths
-csv_file = "customer_shopping_data.csv.csv"
+csv_file = "customer_shopping_data.csv"
 db_file = "mydatabase.db"
 
 # create a DuckDB connection
 conn = duckdb.connect(db_file)
 
 
-'''
-create a table in DuckDB from the CSV file : 
-
-THIS STEP IS DONE ONCE
-
-'''
-#query = f"CREATE TABLE customer_shopping_data AS SELECT * FROM read_csv_auto('{csv_file}')"
-#conn.execute(query)
+query = f"CREATE TABLE if not exists customer_shopping_data AS SELECT * FROM read_csv_auto('{csv_file}')"
+conn.execute(query)
 
 
 # query the data in the table
-#result = conn.execute("SELECT * FROM customer_shopping_data.csv LIMIT 4").fetchall()
-
-# Let's do some querying
-
-insert_into = conn.execute("INSERT INTO customer_shopping_data.csv VALUES ('oumaima', 'cat1' , 'cat11' , 'https://m.media.jpg', 'link.com' , 2 , 4 , 33 , 44 )")
-#select last row
-select_last = conn.execute("SELECT * FROM customer_shopping_data.csv ORDER BY name DESC LIMIT 1;")
-#select
-
-# print the result
-print(insert_into)
-print(select_last)
+result = conn.execute("SELECT * FROM customer_shopping_data LIMIT 4").fetchall()
+print(result)
 
 
